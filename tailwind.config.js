4a29a6a9c5f394ec303ca1225d597998ef9ab289
shovon <shovondas26@gmail.com>
2022-01-28 23:07:02 +0600

    dependencis setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/page/*.{js,jsx,ts,tsx}",
+    "./src/component/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
