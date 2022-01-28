2bc78ed9d9cb5495738ed90741b4f6fc9f483102
shovon <shovondas26@gmail.com>
2022-01-28 23:19:52 +0600

    initial file to start doing with

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import Products from 'views/';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Router>
+          <Switch>
+            <Route path='/' exact component={Products} />
+            <Redirect to="/" />
+          </Switch>
+        </Router>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Tailwind's Compiled output
+import 'styles/output.css'
+// Ant Design
+import "antd/dist/antd.css";
+// My Custom Css
+import "assets/css/style.css";
+
+// App component
+import App from 'App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
