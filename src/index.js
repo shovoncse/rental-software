45c1721b9f9903a31fe1332e3d088e1fc8e30dad
shovon <shovondas26@gmail.com>
2022-01-28 23:35:48 +0600

    web vitals link fixed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import throttle from 'lodash.throttle';
+import reportWebVitals from 'reportWebVitals';
 
 // Tailwind's Compiled output
 import 'styles/output.css'
